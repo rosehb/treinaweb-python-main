a837c2a97304b95b06860c1f0598c7c2fc83ec4d
Rose Brandao <rosehbr@hotmail.com>
2023-01-15 18:30:42 -0500

    Alterando cadastro de usuario

---
+++ administracao/forms/usuario_forms.py
@@ -0,0 +1,15 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import get_user_model
+
+class UsuarioForm(UserCreationForm):
+    class Meta:
+        model = get_user_model()
+        fields = ['username','first_name', 'email', 'password1', 'password2']
+
+    def save(self, commit=True):
+        user = super(UserCreationForm, self).save(commit=False)
+        user.is_superuser = True
+        user.set_password(self.cleaned_data['password1'])
+        if commit:
+            user.save()
+        return user
---
+++ administracao/views/usuario_views.py
@@ -0,0 +1,27 @@
+from ..forms.usuario_forms import UsuarioForm
+from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+
+def cadastrar_usuario(request):
+    if request.method == "POST":
+        form_usuario = UsuarioForm(request.POST)
+        if form_usuario.is_valid():
+            form_usuario.save()
+            return redirect('listar_usuarios')
+    else:
+        form_usuario = UsuarioForm()
+    return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
+
+def listar_usuarios(request):
+    User = get_user_model()
+    usuarios = User.objects.filter(is_superuser=True)
+    return render(request, 'usuarios/lista_usuarios.html', {'usuarios': usuarios})
+
+# def editar_usuario(request, id):
+#     User = get_user_model()
+#     usuario = User.objects.get(id=id)
+#     form_usuario = EditarUsuarioForm(request.POST or None, instance=usuario)
+#     if form_usuario.is_valid():
+#         form_usuario.save()
+#         return redirect('listar_usuarios')
+#     return render(request, 'usuarios/form_usuario.html', {'form_usuario': form_usuario})
